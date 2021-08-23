d7fadad2dcedebefe128f8dc15bad0b8dc8493db
Jinah-engineer <jeeeengineer@gmail.com>
2021-08-23 15:05:32 +0900

    initial commit

---
+++ chatbot_01/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Chatbot01Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chatbot_01'
---
+++ chatbot_01/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+
+import chatbot_01.views
+from . import views
+
+urlpatterns = [
+    path('', chatbot_01.views.home, name="home")
+]
